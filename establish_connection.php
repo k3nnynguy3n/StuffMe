2757b4d9bca9a9cf2cb732a97efce592f010a5e5
Kenny Nguyen <114941124+k3nnynguy3n@users.noreply.github.com>
2024-03-05 16:52:00 -0800

    Add files via upload

---
+++ establish_connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "cssql.seattleu.edu";
+$username = "mj-knguyen26";
+$password = "mj-knguyen261914";
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn){
+    die("Connection failed: " . 
+    mysqli_connect_error());
+}
+echo "Connected successfully";
+
+?>
